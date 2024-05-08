75ff61b3e50f70b4f43701adec1426840617dabd
qapacker <167087295+qapacker@users.noreply.github.com>
2024-05-08 03:08:43 +0200

    Update FizzBuzz.test.js

--- Test/FizzBuzz.test.js
+++ Test/FizzBuzz.test.js
@@ -12,15 +12,4 @@ describe('FizzBuzz function', () => {
     expect(fizzBuzz(10)).toBe('Buzz');
   });
 
-  test('returns "FizzBuzz" when the number is divisible by both 3 and 5', () => {
-    expect(fizzBuzz(15)).toBe('FizzBuzz');
-    expect(fizzBuzz(30)).toBe('FizzBuzz');
-  });
-
-  test('returns the number itself when not divisible by 3 or 5', () => {
-    expect(fizzBuzz(1)).toBe(1);
-    expect(fizzBuzz(2)).toBe(2);
-    expect(fizzBuzz(4)).toBe(4);
-    expect(fizzBuzz(7)).toBe(7);
-  });
-});
+  
