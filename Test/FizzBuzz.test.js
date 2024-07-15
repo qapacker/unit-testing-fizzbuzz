13abedd570d5366ea4276b5b1d15814c3febb25b
Gil, Mateo <mateo.gil@i2cat.net>
2024-07-15 18:02:05 +0200

    cambios en test

--- Test/FizzBuzz.test.js
+++ Test/FizzBuzz.test.js
@@ -1,5 +1,4 @@
-// FizzBuzz.test.js
-import fizzBuzz from '../src/FIzzBuzz.js';
+import fizzBuzz from '../src/FizzBuzz.js';
 
 describe('FizzBuzz function', () => {
   test('returns "Fizz" when the number is divisible by 3', () => {
@@ -12,4 +11,15 @@ describe('FizzBuzz function', () => {
     expect(fizzBuzz(10)).toBe('Buzz');
   });
 
-  
+  test('returns "FizzBuzz" when the number is divisible by both 3 and 5', () => {
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
+    expect(fizzBuzz(30)).toBe('FizzBuzz');
+  });
+
+  test('returns the number itself when not divisible by 3 or 5', () => {
+    expect(fizzBuzz(1)).toBe(1);
+    expect(fizzBuzz(2)).toBe(2);
+    expect(fizzBuzz(4)).toBe(4);
+    expect(fizzBuzz(7)).toBe(7);
+  });
+});
